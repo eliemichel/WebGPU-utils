b8719c9c6422ebd8192ecdd97af3c41b90549cb9
Elie Michel <elie.michel@exppad.com>
2023-05-07 22:19:50 +0200

    Fix arg type in wgpuMaxMipLevels

--- c/wgpuMaxMipLevels.c
+++ c/wgpuMaxMipLevels.c
@@ -2,8 +2,12 @@
 
 #include <assert.h>
 
+#ifndef MAX
+#define MAX(a, b) (a < b ? b : a)
+#endif
+
 // Equivalent of std::bit_width that is available in C++20
-uint32_t bit_width(uint32_t m) {
+static uint32_t bit_width(uint32_t m) {
 	if (m == 0) return 0;
 	else { uint32_t w = 0; while (m >>= 1) ++w; return w; }
 }
@@ -12,7 +16,8 @@ uint32_t bit_width(uint32_t m) {
  * Variant of wgpuMaxMipLevels for when one statically knows that the texture
  * dimension is 1D.
  */
-uint32_t wgpuMaxMipLevels1D(WGPUExtent size) {
+uint32_t wgpuMaxMipLevels1D(WGPUExtent3D size) {
+	(void)size; // Size has indeed no impact
 	return 1;
 }
 
@@ -20,16 +25,16 @@ uint32_t wgpuMaxMipLevels1D(WGPUExtent size) {
  * Variant of wgpuMaxMipLevels for when one statically knows that the texture
  * dimension is 2D.
  */
-uint32_t wgpuMaxMipLevels2D(WGPUExtent size) {
-	return bit_width(std::max(size.width, size.height));
+uint32_t wgpuMaxMipLevels2D(WGPUExtent3D size) {
+	return bit_width(MAX(size.width, size.height));
 }
 
 /**
  * Variant of wgpuMaxMipLevels for when one statically knows that the texture
  * dimension is 3D.
  */
-uint32_t wgpuMaxMipLevels3D(WGPUExtent size) {
-	return bit_width(std::max(size.width, std::max(size.height, size.depthOrArrayLayer)));
+uint32_t wgpuMaxMipLevels3D(WGPUExtent3D size) {
+	return bit_width(MAX(size.width, MAX(size.height, size.depthOrArrayLayers)));
 }
 
 /**
@@ -48,7 +53,7 @@ uint32_t wgpuMaxMipLevels3D(WGPUExtent size) {
  * @return maximum value allowed for `mipLevelCount` in a texture descriptor
  *         that has the given size and dimension.
  */
-uint32_t wgpuMaxMipLevels(WGPUExtent size, WGPUTextureDimension dimension) {
+uint32_t wgpuMaxMipLevels(WGPUExtent3D size, WGPUTextureDimension dimension) {
 	switch (dimension) {
 	case WGPUTextureDimension_1D:
 		return wgpuMaxMipLevels1D(size);
@@ -59,5 +64,6 @@ uint32_t wgpuMaxMipLevels(WGPUExtent size, WGPUTextureDimension dimension) {
 	default:
 		// Should not happen
 		assert(false);
+		return 0;
 	}
 }
